fbed2ac7c4ae0882288deba4fb79a4dde18df4ec
Ipfi Lidzhade <ipfilidzhade@gmail.com>
2019-02-21 16:26:44 +0200

    Added test files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+		
+		static string Prefix(string in_string)
+		{
+			if(String.IsNullOrWhiteSpace(in_string)) return null;
+			
+			int length = in_string.Length;
+			int word_count = in_string.Split().Length;
+			return Convert.ToString(length) + ',' + Convert.ToString(word_count) + ':' + in_string;
+		}
     }
 }
